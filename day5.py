cc6cbf54a9bea7c3cdd4a61b085321a1d0205172
Meithan West <meithan@gmail.com>
2018-12-05 12:31:16 -0600

    Optimizations to Day 5 solution

--- day5.py
+++ day5.py
@@ -3,37 +3,36 @@ from string import ascii_lowercase
 # Read in units
 units = open("day5.in").read().strip()
 
-# Reduced a sequence of units
+# Returns a reduced sequence of units after reactions
 # Exploits the fact that lower- and upper-case letters in ASCII are
 # separated by 32 positions, and that appending or removing an item
-# to/from the end of a list in Python is O(1) (amortized)
+# to/from the end of a list in Python is (amortized) O(1).
 def reduce(units):
   new_units = []
-  for i in range(len(units)):
-    if len(new_units) > 0 and abs(ord(units[i])-ord(new_units[-1])) == 32:
+  for c in [ord(x) for x in units]:
+    if len(new_units) > 0 and abs(c - new_units[-1]) == 32:
       new_units.pop()
     else:
-      new_units.append(units[i])
+      new_units.append(c)
   return new_units
 
 # Part 1
 
-reduced = reduce(units)
-print("Part 1:", len(reduced))
+reduced_units = "".join([chr(x) for x in reduce(units)])
+print("Part 1:", len(reduced_units))
 
 # Part 2
 
+# A big optimization is realzing the result of Part 1 (the shorter
+# reduced list of units) can be used as the starting point for Part 2
 min_len = None
-best_letter = None
 for letter in ascii_lowercase:
 
-  letter_up = letter.upper()
-  units1 = [l for l in units if l != letter and l != letter_up]
-
+  # String replace is fast
+  units1 = reduced_units.replace(letter, "").replace(letter.upper(), "")
   reduced = reduce(units1)
 
   if min_len is None or len(reduced) < min_len:
     min_len = len(reduced)
-    best_letter = letter
 
 print("Part 2:", min_len)
