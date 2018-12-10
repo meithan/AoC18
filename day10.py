46f7a065676bfffb3d3a37297b2f0ac1c8e8b731
Meithan West <meithan@gmail.com>
2018-12-10 11:16:33 -0600

    Improved Day 10 solution (bounding box)

--- day10.py
+++ day10.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 
 plt.ion()
 
-# Represents a particle, its position and speed, a move method
-# and a method to return its squared distance to the origin
+# Represents a particle, its position and speed, and a move method
 class Particle:
   def __init__(self, x, y, vx, vy):
     self.x = x
@@ -16,8 +15,6 @@ class Particle:
   def move(self):
     self.x += self.vx
     self.y += self.vy
-  def dist2(self):
-    return self.x**2 + self.y**2
 
 # Using regex for parsing, just for practice
 pattern = re.compile(r"position=<([ \+\-0-9]+),([ \+\-0-9]+)> velocity=<([ \+\-0-9]+),([ \+\-0-9]+)>")
@@ -29,25 +26,23 @@ with open(sys.argv[1]) as f:
     x, y, vx, vy = [int(x) for x in pattern.match(line).groups()]
     particles.append(Particle(x, y, vx, vy))
 
-# We plot the particles to search for the message only when all
-# particles are within this distance of the origin.
-# Determined by trial and error. Started with 500 and gradually
+# We plot the particles and visually search for the message only when
+# their bounding box has the max of (width, height) within this value
+# Determined by trial and error. Started with 300 and gradually
 # narrowed it down.
-search_dist = 280
+bbox_search_size = 65
 
-# Iterate particle motion and plot only when all particles are
-# within search_dist
+# Iterate particle motion and plot when bounding box is small
 time = 0
 while True:
 
-  # Finding the max of *squared* distances saves an expensive sqrt
-  # for each particle
-  max_dist = sqrt(max([p.dist2() for p in particles]))
-  if max_dist <= search_dist:
-    print(time, max_dist)
+  xs = [p.x for p in particles]
+  ys = [-p.y for p in particles]   # reverse since up is negative
+  bbox_size = max(abs(max(xs)-min(xs)), abs(max(ys)-min(ys)))
+
+  if bbox_size <= bbox_search_size:
+    print(time, bbox_size)
     plt.figure(figsize=(10,2))
-    xs = [p.x for p in particles]
-    ys = [-p.y for p in particles]   # reverse since up is negative
     plt.scatter(xs, ys)
     plt.tight_layout()
     ans = input("Message found? (y/N) ")
@@ -58,7 +53,7 @@ while True:
     p.move()
   time += 1
 
-  if time % 1000 == 0: print(time, max_dist)
+  if time % 1000 == 0: print(time, bbox_size)
 
 print("Part 1: <read message from plot>")
 print("Part 2:", time)
